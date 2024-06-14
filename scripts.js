33a3000e056be05b11cf298633fd97f850e15b48
ceduardooliveira <c-eduardo94@hotmail.com>
2024-06-14 20:45:45 -0300

    Portfolio

--- scripts.js
+++ scripts.js
@@ -5,3 +5,12 @@ document.getElementById("profile-pic").addEventListener("mouseover", function() 
 document.getElementById("profile-pic").addEventListener("mouseout", function() {
     this.style.transform = "rotate(0deg)";
 });
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    const githubLogo = document.getElementById("github");
+
+    githubLogo.addEventListener("click", function() {
+        window.open("https://github.com/ceduardooliveira", "_blank");
+    });
+});
